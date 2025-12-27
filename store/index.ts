1e525ddd8605a6855d9ac58f34b416369a494089
Silmoon Hossain <hssilmoon@gmail.com>
2025-12-27 15:36:10 +0000

    fix: export AppState from types module

--- store/index.ts
+++ store/index.ts
@@ -6,4 +6,5 @@
  * @module store
  */
 
-export { useQuizStore, type QuizState, type AppState, type AnswerRecord } from './quiz-store';
+export { useQuizStore, type QuizState, type AnswerRecord } from './quiz-store';
+export type { AppState } from '@/types/quiz-types';
